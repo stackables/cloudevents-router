0a3116ea7dc148757c461aa769aeae9c494dca28
Aarne Laur <aarne.laur@gmail.com>
2025-08-26 11:38:35 +0200

    feat: Support onError logging in server adapter

--- src/whatwg.ts
+++ src/whatwg.ts
@@ -2,7 +2,11 @@ import { createServerAdapter as csa } from "@whatwg-node/server";
 import { CloudEvent, HTTP } from "cloudevents";
 import { CloudEventsRouter } from ".";
 
-export function getServerAdapter(router: CloudEventsRouter<any>) {
+interface RuntimeOptions {
+	onError: (error: Error) => void;
+}
+
+export function getServerAdapter(router: CloudEventsRouter<any>, ops?: RuntimeOptions) {
 	return csa(async (request: Request) => {
 		if (request.method !== "POST") {
 			return new Response(`Method Not Allowed`, { status: 405 });
@@ -22,13 +26,13 @@ export function getServerAdapter(router: CloudEventsRouter<any>) {
 			// Process the event.
 			await Promise.all(receivedEvent.map((event) => router.process(event)));
 
-			return new Response(`OK`, { status: 200 });
+			return new Response(`OK`, { status: 200, statusText: "OK" });
 		} catch (error: any) {
 			if (error.message === "Not Implemented") {
-				return new Response(`Not Implemented`, { status: 501 });
+				return new Response(`Not Implemented`, { status: 501, statusText: "Not Implemented" });
 			}
-
-			return new Response(`Internal Server Error`, { status: 500 });
+			ops?.onError?.(error);
+			return new Response(`Internal Server Error`, { status: 500, statusText:'Internal Server Error' });
 		}
 	});
 }
